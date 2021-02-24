0711b35db07326f7a6e05ec0e5494ceb3e218b4a
Stovcikova <henrieta.stovcikova@kosickaakademia.sk>
2021-02-24 19:41:14 +0100

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+package sk.kosickaakademia.stovcikova.gitkoxxx;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+    }
+}
---
+++ Main.java
@@ -0,0 +1,15 @@
+package sk.kosickaakademia.stovcikova.gitkoxxx;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String str1 = "Java";
+        String str2 = "PHP";
+
+        System.out.println(str1.length());
+        System.out.println(str2.length());
+        System.out.println("Java".trim().length());
+
+
+    }
+}
